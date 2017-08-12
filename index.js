cbaaa1303eb2de44d5e2950ded5c50855150bb34
Mochamad Taufan Rezzafri <rezzafri@gmail.com>
2017-08-12 09:56:39 +0700

    add introspectionQuery and cache schema

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import {
+  graphql,
+  introspectionQuery,
+} from 'graphql';
 
 import schema from './schema';
 import models from './models';
+
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +19,16 @@ app.use('/graphql', graphqlHTTP({
   context: { models },
   pretty: true,
 }));
+
+graphql(schema, introspectionQuery)
+  .then((result) => {
+    fs.writeFileSync(
+      path.join(__dirname, 'cache/schema.json'),
+      JSON.stringify(result, null, 2),
+    );
+    console.log('Generate cached schema.json file.');
+  })
+  .catch(console.error());
 
 models.sequelize.sync().then(
   () => app.listen(3000),
