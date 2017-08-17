aa69ab6b9726b34fc88fa2c3bac3812551bd9768
Mochamad Taufan Rezzafri <rezzafri@gmail.com>
2017-08-17 16:07:32 +0700

    Remove generated cache

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import {
-  graphql,
-  introspectionQuery,
-} from 'graphql';
-
 import schema from './schema';
 import models from './models';
-
-const fs = require('fs');
-const path = require('path');
 
 const app = express();
 
@@ -19,16 +11,6 @@ app.use('/graphql', graphqlHTTP({
   context: { models },
   pretty: true,
 }));
-
-graphql(schema, introspectionQuery)
-  .then((result) => {
-    fs.writeFileSync(
-      path.join(__dirname, 'cache/schema.json'),
-      JSON.stringify(result, null, 2),
-    );
-    console.log('Generate cached schema.json file.');
-  })
-  .catch(console.error());
 
 models.sequelize.sync().then(
   () => app.listen(3000),
